745cc9407c9bdd1ebaf94381f1eb4bed4447607e
Jacob Persi <me@JacobPersi.com>
2023-05-28 12:19:30 -0400

    Add new engine struct.

--- Structs/StackArray40Int.cs
+++ Structs/StackArray40Int.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BannerlordUnlocked.Structs {
+namespace BannerlordUnlocked {
     public struct StackArray40Int {
         public int this[int index] {
             get {
